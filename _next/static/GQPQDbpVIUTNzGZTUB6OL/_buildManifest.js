b2322221da22b74d26a49fb4ce8036f07c45518c
wgoodall01 <wgoodall01@users.noreply.github.com>
2020-08-04 18:55:30 +0000

    Automated deployment: Tue Aug  4 18:55:30 UTC 2020 2b41ac7f40ec8152cf2d34a5ecbf26e45d3e353c

--- _next/static/GQPQDbpVIUTNzGZTUB6OL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/index":[a]}}("static\u002Fchunks\u002Ffe061be5f1ecfa3536674fa09d374420ffc88ad2.55cea88f0ff7dec0de25.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
